6b66c130854e59312d92a74116c4d22fec27a705
Igor Arruda <igor.arruda@hp.com>
2021-03-12 21:33:06 -0300

    fix backend route in frontend

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -36,7 +36,7 @@ function newBook(book) {
 }
 
 function calculateShipping(id, cep) {
-    fetch('http://localhost:3000/shipping/' + cep)
+    fetch('http://52.226.47.119:3000/shipping/' + cep)
         .then((data) => {
             if (data.ok) {
                 return data.json();
@@ -55,7 +55,7 @@ function calculateShipping(id, cep) {
 document.addEventListener('DOMContentLoaded', function () {
     const books = document.querySelector('.books');
 
-    fetch('http://localhost:3000/products')
+    fetch('http://52.226.47.119:3000/products')
         .then((data) => {
             if (data.ok) {
                 return data.json();
